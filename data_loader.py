18c6c023fff15e2f6bdf157a9bc790bc6fa11a6c
JessieShuyangZhang <zhan270@usc.edu>
2020-09-18 14:23:47 -0700

    Output raw data in csv

--- data_loader.py
+++ data_loader.py
@@ -2,21 +2,29 @@ import sqlite3 as sq3
 # import pandas as pd 
 import numpy as np
 import csv
-import datetime
 
 class DataLoader: 
-    def __init__(self, data_path='../../../../media/raid/jessiezhang/noaa-data.db'):
+    def __init__(self, csv_filename='raw_data.csv', data_path='../../../../media/raid/jessiezhang/noaa-data.db'):
+        '''
+        @type data_path: string
+        @param data_path: the relative path to noaa database
+        @type raw_data: list
+        @param raw_data: an array of tuples that stores time series data
+        @type csv_filename: string
+        @param csv_filename: the name of the csv file storing the raw data
+        '''
+        
         self.data_path = data_path
         self.raw_data = []
+        self.csv_filename = csv_filename
 
     # load a part of time series data
     def load_raw_data(self):
         connector = sq3.connect(self.data_path)
         cursor = connector.cursor()
-        cursor.execute("SELECT position_key,time_of_day,latitude,longitude,depth FROM position WHERE position_key%100=0 AND position_key<=1000000 ORDER BY time_of_day ASC")
+        cursor.execute("SELECT position_key,time_of_day,latitude,longitude,depth FROM position WHERE position_key%100=0 AND position_key<=100000 ORDER BY time_of_day ASC")
         result = cursor.fetchall()
         
-
         # trim the data 
         # remove when time difference with the next is less than 10 minutes
         min_diff = 600 # ten minutes in unix
@@ -31,37 +39,30 @@ class DataLoader:
             result.remove(el)   
 
         # fetch sensor data: conductivity, density, temperature, salinity
-        # for el in result:
-        for i in range(5):
-            fetch_data_cmd = "SELECT value FROM sensor_data WHERE position_key=? AND type_of_data=? LIMIT 1"
-            # position_key = el[0]
+        # prog = 0  # just to show progress
+        for i in range(len(result)):
+            fetch_data_cmd = "SELECT value FROM sensor_data WHERE position_key=? LIMIT 4"
             position_key = result[i][0]
-            cursor.execute(fetch_data_cmd, (position_key,'conductivity'))
-            conductivity = cursor.fetchone()[0]
-            cursor.execute(fetch_data_cmd, (position_key,'density'))
-            density = cursor.fetchone()[0]
-            cursor.execute(fetch_data_cmd, (position_key,'temperature'))
-            temperature = cursor.fetchone()[0]
-            cursor.execute(fetch_data_cmd, (position_key,'salinity'))
-            salinity = cursor.fetchone()[0]
+            cursor.execute(fetch_data_cmd, (position_key,))
+            sensor_values = []
+            for x in range(4):
+                sensor_values.append(cursor.fetchone()[0])
             temp = list(result[i])
-            temp.extend([conductivity, density, temperature, salinity])
+            temp.extend(sensor_values)
             result[i] = tuple(temp)
-            
+            # print(prog)
+            # prog+=1
 
         cursor.close()
         connector.close()
         self.raw_data = result
-        # return result
 
-    def output_csv():
-        pass
+    def output_csv(self):
+        if not self.raw_data:
+            self.load_raw_data()
+        with open(self.csv_filename, 'wb') as csvfile:
+            csv_out = csv.writer(csvfile)
+            csv_out.writerow(['postion_key','unix_time','latitude','longitude','depth','conductivity','density','temperature','salinity'])
+            csv_out.writerows(self.raw_data)
 
-    
-
-
-'''
-import datetime
-timestamp = datetime.datetime.fromtimestamp(1500000000)
-print(timestamp.strftime('%Y-%m-%d %H:%M:%S'))
-'''
+        print("done with csv")
